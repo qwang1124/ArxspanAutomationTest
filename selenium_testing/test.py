5e3309ed9f7395dc49aac47e73c7130599bca28b
schen2011 <schenjobs@gmail.com>
2019-01-03 21:56:46 -0500

    check in examples to get elements by tag

--- selenium_testing/test.py
+++ selenium_testing/test.py
@@ -1,9 +1,29 @@
 from selenium import webdriver
 # first "pip install webdriver-manager"
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('http://localhost:8000')
-driver.find_element_by_id('demo').click()
+driver.get('https://model.arxspan.com/login.asp')
+driver.maximize_window()
+driver.find_element_by_id('login-email').send_keys('admin@demo.com')
+driver.find_element_by_id('login-pass').send_keys('carbonCopee')
+driver.find_element_by_id('login-submit').send_keys(Keys.RETURN)
+select = Select(driver.find_element_by_tag_name('select'))
+select.select_by_visible_text('Demo')
+driver.find_element_by_id('login-submit').send_keys(Keys.ENTER)
+
+
+elms = driver.find_elements_by_tag_name('a')
+for elm in elms:
+    if elm.text == 'Notebooks':
+        elm.click()
+        links = driver.find_elements_by_tag_name('a')
+        for link in links:
+            if link.text.trim == ' Test_Notebook_QingWang':
+                link.click()
+
+
 
